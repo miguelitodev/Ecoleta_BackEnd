74475dbe1b346a71437eec1bc484b54b33118e7f
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-06-08 00:36:03 -0300

    Upload de imagem ok

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -32,7 +32,10 @@ class PointsController {
 
 		const point_id = insertedIds[0];
 
-		const pointItems = items.map((item_id: number) => {
+		const pointItems = items
+			.split(',')
+			.map((item: string) => Number(item.trim()))
+			.map((item_id: number) => {
 			return {
 				item_id,
 				point_id,
